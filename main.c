3321a3fa28c4f885d9ce8e17257e05858f2fca9f
matikosowy <88056938+matikosowy@users.noreply.github.com>
2023-12-11 21:54:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -57,11 +57,10 @@ void unvisitAllCells(cell *cells, int cellCount) {
 // LOSOWE PRZYPISANIE WAG KOMORKOM LABIRYNTU
 void assignWeightsToCells(cell *cells, int cellCount) {
     srand(time(NULL));
-
+    
     for (int i = 0; i < cellCount; i++) {
         int randomInt = rand()%1001;
         double randomDouble = (double)randomInt / 100.0;
-        
         cells[i].weight = randomDouble;
     }
 }
@@ -163,36 +162,30 @@ void setNeighbours(cell *cells, int cellCount, int cellWidth) {
 
     for (int i = 0; i < cellCount; i++) {
         int j = 0;
-        // printf("%d: ", cells[i].id);
         int up = i - cellWidth;
         if (isCellValid(cells[up], cellCount)) {
             cells[i].neighbours[j] = &cells[up];
-            // printf("u - %d, ",cells[i].neighbours[j]->id);
             j++;
         }
 
         int right = i + 1;
         if (isCellValid(cells[right], cellCount)) {
             cells[i].neighbours[j] = &cells[right];
-            // printf("r - %d, ",cells[i].neighbours[j]->id);
             j++;
         }
 
         int down = i + cellWidth;
         if (isCellValid(cells[down], cellCount)) {
             cells[i].neighbours[j] = &cells[down];
-            // printf("d - %d, ",cells[i].neighbours[j]->id);
             j++;
         }
 
         int left = i - 1;
         if (isCellValid(cells[left], cellCount)) {
             cells[i].neighbours[j] = &cells[left];
-            // printf("l - %d, ",cells[i].neighbours[j]->id);
             j++;
         }
         cells[i].neighboursCount = j;
-        // printf("\n");
     }
 }
 
@@ -240,7 +233,6 @@ void setCellAsVisitedByCoordinates(cell *cells, int x, int y, int cellCount){
     for(int i=0; i<cellCount; i++){
         if(cells[i].x == x && cells[i].y == y){
             cells[i].isVisited = 1;
-            //printf("set as visited %d\n", cells[i].id);
         }
     }
 }
@@ -304,7 +296,6 @@ void printGraph(struct Graph* graph, cell *cells, int cellCount) {
             temp = temp->next;
         }
         printf("\n");
-        //printf("NULL\n\n");
     }
 }
 
@@ -327,7 +318,6 @@ void setCellAsVisitedById(cell *cells, int id, int cellCount){
     for(int i=0; i<cellCount; i++){
         if(cells[i].id == id){
             cells[i].isVisited = 1;
-            //printf("set as visited %d\n", cells[i].id);
         }
     }
 }
@@ -358,91 +348,10 @@ bool isWallBetween(cell current, cell adjacent, char maze[MAX_SIZE][MAX_SIZE]){
     return 1;
 }
 
-// NIE DZIALA
-void mazeIntoGraph(cell *cells, char maze[MAX_SIZE][MAX_SIZE], struct Graph* graph, int cellCount, int cellWidth){
-    unvisitAllCells(cells, cellCount);
-
-    // PRZYPISANIE KOMORKOM SASIADOW
-    setNeighbours(cells, cellCount, cellWidth);
-
-    // INICJALIZACJA STOSU
-    cell *stack = malloc(MAX_SIZE * sizeof(cell)); 
-    int top = -1;
-
-    // DODANIE NA STOS KOMORKI STARTOWEJ
-    for(int i=0; i<cellCount; i++){
-        if(cells[i].isStart == 1){
-            stack[++top] = cells[i];
-            break;
-        }
-    }
-
-    cell previous = cells[cellCount-1];
-
-    while(top>=0){
-        cell current = stack[top];
-
-        if(current.id != previous.id && !isWallBetween(previous, current, maze)){
-            addEdge(graph, previous.id, current.id, current.weight);
-        }
-        
-        setCellAsVisitedById(cells, current.id, cellCount);
-        
-        int neighboursCount = current.neighboursCount;
-
-        for(int i=0; i<neighboursCount; i++){
-            if(current.neighbours[i] != NULL && current.neighbours[i]->isVisited == 0){
-                stack[++top] = *(current.neighbours[i]);
-                
-            }
-        }
-        
-        if(!hasUnvisitedNeighbour(current, cellCount) || current.isEnd){
-            top--;
-        }
-        previous = current;
-        
-    }
-    free(stack);
-}
-
-// GENEROWANIE LISTY SASIEDZTWA GRAFU
-void adjacencyListGen(char maze[MAX_SIZE][MAX_SIZE], int mazeSize, int cellCount, cell *cells, struct Graph* graph) {
-    for(int i=1; i<mazeSize-1; i+=2){
-        for(int j=1; j<mazeSize-1; j+=2){
-            if(maze[i-1][j] == ' ' && i > 1){
-                cell src = returnCellByCoordinates(cells, i, j, cellCount);
-                cell dst = returnCellByCoordinates(cells, i-2, j, cellCount);
-
-                addEdge(graph, src.id, dst.id, dst.weight);
-            }
-            if(maze[i+1][j] == ' ' && i < mazeSize-2){
-                cell src = returnCellByCoordinates(cells, i, j, cellCount);
-                cell dst = returnCellByCoordinates(cells, i+2, j, cellCount);
-
-                addEdge(graph, src.id, dst.id, dst.weight);
-            }
-            if(maze[i][j-1] == ' ' && j > 1){
-                cell src = returnCellByCoordinates(cells, i, j, cellCount);
-                cell dst = returnCellByCoordinates(cells, i, j-2, cellCount);
-
-                addEdge(graph, src.id, dst.id, dst.weight);
-            }
-            if(maze[i][j+1] == ' ' && j < mazeSize-2){
-                cell src = returnCellByCoordinates(cells, i, j, cellCount);
-                cell dst = returnCellByCoordinates(cells, i, j+2, cellCount);
-
-                addEdge(graph, src.id, dst.id, dst.weight);
-            }
-        }
-    }
-}
-
 // LOSOWE OTWIERANIE SCIEZEK W LABIRYNCIE + GENEROWANIE GRAFU LABIRYNTU
 void randomPathOpenAndGraphGen(cell *cells, char maze[MAX_SIZE][MAX_SIZE], int mazeSize, int cellCount, int cellWidth, struct Graph* graph){
     srand(time(NULL));
     int randomCellIndex = rand()%cellCount;
-    //printf("cell start - %d\n", cells[randomCellIndex].id);
     
     for(int i=0; i<cellCount; i++){
         if(cells[i].isStart){
@@ -463,7 +372,6 @@ void randomPathOpenAndGraphGen(cell *cells, char maze[MAX_SIZE][MAX_SIZE], int m
         switch(direction){
             // GORA
             case 0:
-                //printf("gora\n");
                 move = current.x - 2;
                 if(move > 0){
                     next = returnCellByCoordinates(cells, move, current.y, cellCount);
@@ -476,7 +384,6 @@ void randomPathOpenAndGraphGen(cell *cells, char maze[MAX_SIZE][MAX_SIZE], int m
                 break;
             // PRAWO
             case 1:
-                //printf("prawo\n");
                 move = current.y + 2;
                 if(move<mazeSize-1){
                     next = returnCellByCoordinates(cells, current.x, move, cellCount);
@@ -489,7 +396,6 @@ void randomPathOpenAndGraphGen(cell *cells, char maze[MAX_SIZE][MAX_SIZE], int m
                 break;
             // DOL
             case 2:
-                //printf("dol\n");
                 move = current.x + 2;
                 if(move<mazeSize-1){
                     next = returnCellByCoordinates(cells, move, current.y, cellCount);
@@ -502,7 +408,6 @@ void randomPathOpenAndGraphGen(cell *cells, char maze[MAX_SIZE][MAX_SIZE], int m
                 break;
             // LEWO
             case 3:
-                //printf("lewo\n");
                 move = current.y - 2;
                 if(move>0){
                     next = returnCellByCoordinates(cells, current.x, move, cellCount);
@@ -516,7 +421,6 @@ void randomPathOpenAndGraphGen(cell *cells, char maze[MAX_SIZE][MAX_SIZE], int m
             default:
                 break;
         }
-        //printf("current: %d\n", current.id);
     }
     return;
 }
@@ -554,9 +458,6 @@ int main() {
     randomPathOpenAndGraphGen(cells, maze, mazeSize, cellCount, cellWidth, graph); // OTWIERANIE LOSOWO SCIEZEK W LABIRYNCIE + LISTA SASIEDZTWA
 
     mazePrint(maze, mazeSize); // WYSWIETL LABIRYNT
-
-    //mazeIntoGraph(cells, maze, graph, cellCount, cellWidth);
-    //adjacencyListGen(maze, mazeSize, cellCount, cells, graph); // UTWORZ LISTE SASIEDZTWA
 
     printGraph(graph, cells, cellCount); // WYSTWIETL GRAF
 
